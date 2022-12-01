9f0d49915ea4f0c4a4d7c2459927b9c44097627c
Nikita Syutkin <syutkin.program@gmail.ru>
2022-12-01 18:51:51 +0700

    first commit

---
+++ index.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int a = 4, b = 5
+	cout<<"Hello Git"<<endl;
+	cout<<a + b<<endl;
+	return 0;
+}
---
+++ script.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	cout<<Script<<endl;
+	return 0;
+}
